3629588d0e3960b2e0f7f71b022b80f3eb8ca6e7
beaever <thebeaever@gmail.com>
2021-11-20 15:27:23 +0900

    temp commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+// Style
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+	// 기본적으로 선언할 내장함수
 	const router = useRouter();
-
+	// useState
+	const [state, setState] = useState<any>(""); // 임시 예제
+	// Function
 	const onMove = () => {
 		router.push("/login");
 	};
 
+	// useEffect
+	useEffect(() => {}, []);
+
+	// RETURN
 	return <div onClick={() => onMove()}>HOME</div>;
 }
