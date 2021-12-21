e7958049a777f90564f8b51ab33325d97fd9789f
beaever <thebeaever@gmail.com>
2021-12-22 00:27:08 +0900

    dependencies remove node-sass

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/dist/client/router';
 import API from '../../service/api';
 import UserMemberInfoResponseDataModel from '../../service/api/user/model/user-member-info-response-data-model';
-import Link from 'next/link'
-
-import MainButton from "../../components/button/main-button"; 
+import Link from 'next/link';
 
 const Login = () => {
 	const router = useRouter();
@@ -14,7 +12,7 @@ const Login = () => {
 	};
 
 	const GoSnsJoin = () => {
-     router.push('/join/snsjoin')
+		router.push('/join/snsjoin');
 	};
 
 	const getMemberInfo = () => {
@@ -24,7 +22,7 @@ const Login = () => {
 				const data = res.data as UserMemberInfoResponseDataModel;
 				if (data.code === 200) {
 					console.log(data.data);
-					router.push("/")
+					router.push('/');
 				} else {
 					alert(data.msg);
 				}
@@ -56,18 +54,18 @@ const Login = () => {
 							<input className="loginInput" type="password" />
 							<button className="login_btn">로그인</button>
 							<div className="sns_btn">
-								<button onClick={()=> GoSnsJoin()}>카카오</button>
-								<button onClick={()=> GoSnsJoin()}>구글</button>
-								<button onClick={()=> GoSnsJoin()}>네이버</button>
+								<button onClick={() => GoSnsJoin()}>카카오</button>
+								<button onClick={() => GoSnsJoin()}>구글</button>
+								<button onClick={() => GoSnsJoin()}>네이버</button>
 							</div>
 							<div className="util_btn">
 								<button onClick={() => onMove()}>회원가입</button>
-							<Link href='/find' as="/find">
+								<Link href="/find" as="/find">
 									<a>
-										<button >아이디찾기</button>
+										<button>아이디찾기</button>
 										<button>비밀번호찾기</button>
 									</a>
-							</Link>
+								</Link>
 							</div>
 						</div>
 					</div>
