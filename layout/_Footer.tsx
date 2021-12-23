3782c9ccac430ebc518b75b1b42c706f4b203b22
slobbie <haeru9410@gmail.com>
2021-12-24 04:38:30 +0900

    pcHeader , pcFooter 레이아웃수정

--- layout/_Footer.tsx
+++ layout/_Footer.tsx
@@ -3,48 +3,17 @@ const Footer = () => {
   return (
     <footer className="footer">
         <div className="footerinner">
-            {/* <div className="service_area">
-                <address className="footeraddr">
-                    <h3>
-                        <a href="tel:1588-xxxx">고객센터 1588-xxxx</a>
-                    </h3>
-                    <p>운영시간평일 11:00 - 18:00 (토 ∙ 일, 공휴일 휴무)</p>
-                    <p>점심시간평일 13:00 - 14:00</p>
-                    <span>1:1 문의하기는 앱에서만 가능합니다.</span><br />
-                    <button type="button">
-                        <a href="/">자주 묻는 질문</a>
-                    </button>
-                </address>      
-                <div className="footerbox">
-                    <h3>이용안내</h3>
-                    <ul>
-                        <li><a href="/">검수기준</a></li>
-                        <li><a href="/">이용정책</a></li>
-                        <li><a href="/">패널티정책</a></li>
-                    </ul>
-                </div>
-                <div className="footerbox">
-                    <h3>고객안내</h3>
-                    <ul>
-                        <li><a href="/">공지사항</a></li>
-                        <li><a href="/">서비스소개</a></li>
-                        <li><a href="/">판매자 방문접수</a></li>
-                    </ul>
-                </div>
-
-                        
-            </div> */}
+            <h2>GOING BUYING</h2>
             <div className="corporation_area">
                 <ul>
-                    <li><a href="/">회사소개</a></li>
-                    <li><a href="/">인재채용</a></li>
+                    <li><a href="/">공지사항</a></li>
                     <li><a href="/">이용약관</a></li>
-                    <li><a href="/">개인정보처리</a></li>        
+                    <li className="aferNone"><a href="/">개인정보처리방침</a></li>        
                 </ul>
             </div>
             <div className="copyright">
-                <p> 통신판매 중개자로서 통신판매의 당사자가 아니므로 개별 판매자가 등록한 상품정보에 대해서 책임을 지지 않습니다. 단, 거래과정에서 검수하고 보증하는 내용에 대한 책임은 당사에 있습니다.</p>
-                <p>© 2021 COPY RIGHT</p>
+                <p>회사명 : 고잉바잉  |   대표 : 홍길동   |   주소 : 서울특별시 노원구 동일로215길 48   |   사업자번호 : 111-11-11111</p>
+                <p>Copyright 2022. WIT. All rights reserved</p>
             </div>
         </div>
     </footer>
