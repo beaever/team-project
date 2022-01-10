e6f2c3ebb4b25660b0e166f018b0a6c25e26b799
slobbie <haeru9410@gmail.com>
2022-01-11 04:55:34 +0900

    공지사항 수정

--- pages/notice/index.tsx
+++ pages/notice/index.tsx
@@ -1,118 +1,136 @@
 import Link from 'next/link';
 import { useState } from 'react';
 
+import { useRouter } from 'next/router';
+import React from 'react';
+import MarginBottom from '../../components/layout/margin-bottom';
+import MarginTop from '../../components/layout/margin-top';
+import useWidth from '../../hooks/useWitdh';
+import Footer from '../../layout/_Footer';
+import MobileFooter from '../../layout/_mobileFooter';
+import MobileHeader from '../../layout/_mobileHeader';
+import PcHeader from '../../layout/_pcHeader';
+
 interface NoticeListDataModel {
-	idx: number;
-	title: string;
+  idx: number;
+  title: string;
 }
 
 const Notice = () => {
-	const [noticeList, setNoticeList] = useState<NoticeListDataModel[]>([
-		{
-			idx: 1,
-			title: '2022 공지사항입니다.',
-		},
-		{
-			idx: 2,
-			title: '2022 공지사항입니다.1',
-		},
-		{
-			idx: 3,
-			title: '2022 공지사항입니다.2',
-		},
-		{
-			idx: 4,
-			title: '2022 공지사항입니다.3',
-		},
-		{
-			idx: 5,
-			title: '2022 공지사항입니다.4',
-		},
-		{
-			idx: 6,
-			title: '2022 공지사항입니다.',
-		},
-		{
-			idx: 7,
-			title: '2022 공지사항입니다.1',
-		},
-		{
-			idx: 8,
-			title: '2022 공지사항입니다.2',
-		},
-		{
-			idx: 9,
-			title: '2022 공지사항입니다.3',
-		},
-		{
-			idx: 10,
-			title: '2022 공지사항입니다.4',
-		},
-		{
-			idx: 11,
-			title: '2022 공지사항입니다.',
-		},
-		{
-			idx: 12,
-			title: '2022 공지사항입니다.1',
-		},
-		{
-			idx: 13,
-			title: '2022 공지사항입니다.2',
-		},
-		{
-			idx: 14,
-			title: '2022 공지사항입니다.3',
-		},
-		{
-			idx: 15,
-			title: '2022 공지사항입니다.4',
-		},
-		{
-			idx: 16,
-			title: '2022 공지사항입니다.',
-		},
-		{
-			idx: 17,
-			title: '2022 공지사항입니다.1',
-		},
-		{
-			idx: 18,
-			title: '2022 공지사항입니다.2',
-		},
-		{
-			idx: 19,
-			title: '2022 공지사항입니다.3',
-		},
-		{
-			idx: 20,
-			title: '2022 공지사항입니다.4',
-		},
-	]);
+  const { mediaQuery } = useWidth();
+  const [noticeList, setNoticeList] = useState<NoticeListDataModel[]>([
+    {
+      idx: 1,
+      title: '2022 공지사항입니다.',
+    },
+    {
+      idx: 2,
+      title: '2022 공지사항입니다.1',
+    },
+    {
+      idx: 3,
+      title: '2022 공지사항입니다.2',
+    },
+    {
+      idx: 4,
+      title: '2022 공지사항입니다.3',
+    },
+    {
+      idx: 5,
+      title: '2022 공지사항입니다.4',
+    },
+    {
+      idx: 6,
+      title: '2022 공지사항입니다.',
+    },
+    {
+      idx: 7,
+      title: '2022 공지사항입니다.1',
+    },
+    {
+      idx: 8,
+      title: '2022 공지사항입니다.2',
+    },
+    {
+      idx: 9,
+      title: '2022 공지사항입니다.3',
+    },
+    {
+      idx: 10,
+      title: '2022 공지사항입니다.4',
+    },
+    {
+      idx: 11,
+      title: '2022 공지사항입니다.',
+    },
+    {
+      idx: 12,
+      title: '2022 공지사항입니다.1',
+    },
+    {
+      idx: 13,
+      title: '2022 공지사항입니다.2',
+    },
+    {
+      idx: 14,
+      title: '2022 공지사항입니다.3',
+    },
+    {
+      idx: 15,
+      title: '2022 공지사항입니다.4',
+    },
+    {
+      idx: 16,
+      title: '2022 공지사항입니다.',
+    },
+    {
+      idx: 17,
+      title: '2022 공지사항입니다.1',
+    },
+    {
+      idx: 18,
+      title: '2022 공지사항입니다.2',
+    },
+    {
+      idx: 19,
+      title: '2022 공지사항입니다.3',
+    },
+    {
+      idx: 20,
+      title: '2022 공지사항입니다.4',
+    },
+  ]);
 
-	return (
-		<section className="noticePage">
-			<div className="noticeContainer">
-				<h2>공지사항</h2>
-				<div>
-					<ul className="notice_ul">
-						{noticeList?.map((item) => {
-							return (
-								<>
-									<Link href="/notice/board" as="/notice/board">
-										<a>
-											<li key={item.idx}>{item.title}</li>
-										</a>
-									</Link>
-								</>
-							);
-						})}
-					</ul>
-					{noticeList.length > 10 && <button> + 더보기</button>}
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <>
+      {mediaQuery === 'M' ? <MobileHeader /> : <PcHeader />}
+      <MarginTop margin={100} />
+      <section className='noticePage'>
+        <h2 className='noticePage_h2'>공지사항</h2>
+        <div className='noticeContainer'>
+          <MarginBottom margin={40} />
+          <div>
+            <ul className='notice_ul'>
+              {noticeList?.map((item) => {
+                return (
+                  <>
+                    <Link href='/notice/board' as='/notice/board'>
+                      <a>
+                        <li key={item.idx}>{item.title}</li>
+                      </a>
+                    </Link>
+                  </>
+                );
+              })}
+            </ul>
+            {noticeList.length > 10 && <button> + 더보기</button>}
+          </div>
+        </div>
+      </section>
+      {mediaQuery === 'M' ? <MobileFooter /> : <Footer />}
+      <MarginBottom margin={100} />
+    </>
+  );
 };
 
 export default Notice;
