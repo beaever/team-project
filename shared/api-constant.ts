8320971d7f6ee8ec653faf68e04f0e3ec1e4ebdb
beaever <thebeaever@gmail.com>
2022-02-03 23:36:08 +0900

    api-constant.ts modify

--- shared/api-constant.ts
+++ shared/api-constant.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://3.37.125.107'; // => BACKEND API URL
+export const BASE_URL = 'http://3.37.125.107/api/v1/'; // => BACKEND API URL
+export const AUTH_URL = 'auth/';
 export const JSON_HEADER = { 'Content-type': 'application/json' };
 export const FORM_HEADER = { 'Content-type': 'multipart/form-data' };
 export const GET = 'GET';
