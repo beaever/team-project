ad4542a8f1045e18ab1e74db2ae268592d3b0107
beaever <thebeaever@gmail.com>
2022-02-19 17:17:56 +0900

    terms, join, terms.ts modify

--- service/api/user/user.ts
+++ service/api/user/user.ts
@@ -1,36 +1,36 @@
 import axios from 'axios';
-import { request } from 'http';
-import { AUTH_URL, GET, JSON_HEADER, POST } from '../../../shared/api-constant';
+import { BASE_URL, GET, JSON_HEADER, POST } from '../../../shared/api-constant';
 import { AxiosRequestConfig } from '../config/axios';
-import UserMemberInfoRequestDataModel from './model/user-member-info-request-data-model';
+import UserCheckEmailRequesetDataModel from './model/user-checkEmail-request-data-model';
 
 export const user = {
   // 회원의 개인 정보 가져오기 API
-  memberInfo: (request: UserMemberInfoRequestDataModel) => {
-    const config: AxiosRequestConfig = {
-      url: AUTH_URL + 'check-email',
-      method: POST, // GET 방식이라면 GET , POST 방식이라면 POST ,
-      headers: {
-        ...JSON_HEADER,
-        // 토큰 값을 원한다면 Authorization: Beazie or  token,
-      },
-      data: {
-        // POST 방식에서 요구하는 Params value
-        email: request.email ?? '',
-      },
-    };
-    return axios(config);
-  },
+  // memberInfo: (request: UserMemberInfoRequestDataModel) => {
+  //   const config: AxiosRequestConfig = {
+  //     url: 'check-email',
+  //     method: POST, // GET 방식이라면 GET , POST 방식이라면 POST ,
+  //     headers: {
+  //       ...JSON_HEADER,
+  //       // 토큰 값을 원한다면 Authorization: Beazie or  token,
+  //     },
+  //     data: {
+  //       // POST 방식에서 요구하는 Params value
+  //       email: request.email ?? '',
+  //     },
+  //   };
+  //   return axios(config);
+  // },
+
   // 이메일 중복 검사 API
-  checkEmail: (reqEmail: string) => {
+  checkEmail: (request: UserCheckEmailRequesetDataModel) => {
     const config: AxiosRequestConfig = {
-      url: 'check-email',
+      url: BASE_URL + 'auth/check-email',
       method: POST,
       headers: {
         ...JSON_HEADER,
       },
       data: {
-        email: reqEmail,
+        email: request.email ?? '',
       },
     };
     return axios(config);
